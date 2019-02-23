5e99f9169b9cc41e277e0e4eeb9820e7b9f3e1a0
Philippe Gerum <rpm@xenomai.org>
2019-02-23 15:34:58 +0100

    tests: logger: convert to proxy element

--- tests/logger-stdout.c
+++ tests/logger-stdout.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
-#include <evenless/logger.h>
+#include <evenless/proxy.h>
 #include <evenless/thread.h>
 
 int main(int argc, char *argv[])
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 
 	efd = evl_attach_self("simple-bind:%d", getpid());
 	printf("efd=%d\n", efd);
-	logfd = evl_new_logger(fileno(stdout), 16);
+	logfd = evl_new_proxy(fileno(stdout), 16, "stdout:%d", getpid());
 	printf("logfd=%d\n", logfd);
 	ret = oob_write(logfd, "stdout relay!\n", 14);
 	printf("oob_write=%d, errno=%d\n", ret, errno);
