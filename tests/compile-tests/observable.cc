654717f07e2ba19ca09724fdcddd1f7c36a01be3
Philippe Gerum <rpm@xenomai.org>
2020-05-04 11:21:20 +0200

    tests: compile/observable: add missing checks
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- tests/compile-tests/observable.cc
+++ tests/compile-tests/observable.cc
@@ -8,8 +8,14 @@
 
 int main(int argc, char *argv[])
 {
+	struct evl_notification nf;
+	struct evl_notice no;
+	int efd;
+
 	evl_new_observable("test");
-	evl_create_observable(EVL_CLONE_PRIVATE, "test");
+	efd = evl_create_observable(EVL_CLONE_PRIVATE, "test");
+	evl_update_observable(efd, &no, 1);
+	evl_read_observable(efd, &nf, 1);
 
 	return 0;
 }
