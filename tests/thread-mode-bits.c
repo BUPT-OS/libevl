57b78404a1bd229cbaa581a971456647b2cf60e4
Philippe Gerum <rpm@xenomai.org>
2020-11-10 16:04:14 +0100

    tests: thread-mode-bits: fix regression (lingering debug code)
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- tests/thread-mode-bits.c
+++ tests/thread-mode-bits.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 	__Tcall_assert(tfd, evl_attach_self("thread-mode-bits:%d", getpid()));
 
 	/* Starts with no mode bit set. */
-	__Tcall_assert(ret, evl_set_thread_mode(tfd, /*T_WOSS|*/T_WOLI|T_WOSX, &oldmask));
+	__Tcall_assert(ret, evl_set_thread_mode(tfd, T_WOSS|T_WOLI|T_WOSX, &oldmask));
 	/* T_WOLI may be preset if CONFIG_EVL_DEBUG_WOLI is set. */
 	__Texpr_assert((oldmask & ~T_WOLI) == 0);
 	__Tcall_assert(ret, evl_set_thread_mode(tfd, 0, &oldmask));
