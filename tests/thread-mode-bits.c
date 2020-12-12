f6587742b75341afb77bef6fa2e599d888690abc
Philippe Gerum <rpm@xenomai.org>
2020-12-12 19:03:49 +0100

    tests: thread-mode-bits: T_WOLI might be preset - take #2
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- tests/thread-mode-bits.c
+++ tests/thread-mode-bits.c
@@ -22,9 +22,11 @@ int main(int argc, char *argv[])
 
 	__Tcall_assert(tfd, evl_attach_self("thread-mode-bits:%d", getpid()));
 
-	/* Starts with no mode bit set. */
+	/*
+	 * Starts with no mode bit set, except maybe T_WOLI if
+	 * CONFIG_EVL_DEBUG_WOLI is enabled.
+	 */
 	__Tcall_assert(ret, evl_set_thread_mode(tfd, T_WOSS|T_WOLI|T_WOSX, &oldmask));
-	/* T_WOLI may be preset if CONFIG_EVL_DEBUG_WOLI is set. */
 	__Texpr_assert((oldmask & ~T_WOLI) == 0);
 	__Tcall_assert(ret, evl_set_thread_mode(tfd, 0, &oldmask));
 	__Texpr_assert(oldmask == (T_WOSS|T_WOLI|T_WOSX|T_HMSIG));
@@ -41,7 +43,7 @@ int main(int argc, char *argv[])
 						"thread-mode-bits:%d", getpid()));
 
 	__Tcall_assert(ret, evl_set_thread_mode(tfd, T_WOSS|T_WOLI|T_WOSX|T_HMOBS, &oldmask));
-	__Texpr_assert(oldmask == 0);
+	__Texpr_assert((oldmask & ~T_WOLI) == 0);
 	__Tcall_assert(ret, evl_set_thread_mode(tfd, T_HMSIG, &oldmask));
 	__Texpr_assert(oldmask == (T_WOSS|T_WOLI|T_WOSX|T_HMOBS));
 	__Tcall_assert(ret, evl_clear_thread_mode(tfd, T_HMSIG|T_HMOBS, &oldmask));
